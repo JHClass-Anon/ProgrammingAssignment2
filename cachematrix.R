a407643a07ed71b6220627fccf59642c7cf1addc
JHClass-Anon <oxbow-26463@mypacks.net>
2015-01-17 20:41:14 -0500

    Code file for Project 2 - R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,77 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## Create a list of member functions for a user defined vector wrapper
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function(inverse) m
+  
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse= getInverse)
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Returns a matrix inverse or the cached value of the matrix inverse
+cacheSolve <- function(x) {
+  
+  inv <- x$getInverse()
+  
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  ## Return a matrix that is the inverse of 'x'
+  cacheInv <- function(x) {
+    return(solve(x))
+  }
+  
+  print("Computing and storing matrix")
+  inv <- cacheInv(x$get())
+  x$setInverse(inv)
+    
+  inv
 }
+
+
+
+##TESTING CODE
+##TEST MATRIX
+mtrxExample<- matrix(c(.5,-.5,.5,.5),nrow=2,byrow=TRUE)
+
+#SET MATRIX
+testMatrix <- makeCacheMatrix()
+testMatrix$set(mtrxExample)
+
+#GET MATRIX - Should be example mtrxExample matrix
+if (identical(testMatrix$get(), mtrxExample))
+    print("Matrix Set works properly")
+
+#GET INVERSE - Not yet set so this should compute it and store
+#Inverse should be (.5,-.5,.5,.5)
+
+cacheSolve(testMatrix)
+if (identical(testMatrix$getInverse(),matrix(c(.5,-.5,.5,.5), nrow=2,byrow=TRUE)))
+  print("Matrix store and Inversion calculation work properly")
+
+#SHOULD BE ABLE TO RESET AND RECOMPUTE/STORE AND RETURN ORIGINAL MATRIX
+mtrxExample<- matrix(c(.5,-.5,.5,.5),nrow=2,byrow=TRUE)
+testMatrix$set(mtrxExample)
+cacheSolve(testMatrix)
+if (identical(testMatrix$get(), mtrxExample))
+  print("Matrix reuse works properly")
+
+
+
+
+
+
+  
